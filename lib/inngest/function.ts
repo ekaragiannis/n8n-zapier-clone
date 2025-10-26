fd11078fd527b4df73607c96090dacd3b3aed112
Efthymios Karagiannis <efthymioskaragiannis@gmail.com>
2025-10-27 00:51:18 +0200

    add error tracking with sentry

--- lib/inngest/function.ts
+++ lib/inngest/function.ts
@@ -1,14 +1,22 @@
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import * as Sentry from "@sentry/nextjs";
 import { generateText } from "ai";
 import { inngest } from "./client";
 
+Sentry.logger.info("User triggered test log", { log_source: "sentry_test" });
 const google = createGoogleGenerativeAI();
 
 export const execute = inngest.createFunction({ id: "execute" }, { event: "execute/ai" }, async ({ event, step }) => {
+  Sentry.logger.info("User triggered test log", { log_source: "sentry_test" });
   const { steps } = await step.ai.wrap("gemini-generate-text", generateText, {
     system: "You are a helpful assistant",
     prompt: "What is 2+2?",
     model: google("gemini-2.5-flash"),
+    experimental_telemetry: {
+      isEnabled: true,
+      recordInputs: true,
+      recordOutputs: true,
+    },
   });
 
   return steps;
